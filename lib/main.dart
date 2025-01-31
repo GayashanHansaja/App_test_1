de4ec8b28ad49cb5bc4741116833e76925feb3ae
IT22171856 <142132191+IT22171856@users.noreply.github.com>
2025-01-31 14:11:36 +0530

    add mars and pluto section,gesturedetected applied

--- lib/main.dart
+++ lib/main.dart
@@ -9,19 +9,155 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'test app 1',
         home: Scaffold(
           backgroundColor: Colors.black,
           appBar: AppBar(
-            backgroundColor: Colors.white,
+            backgroundColor: Colors.black,
             shadowColor: Colors.white,
             title: const Text(
               'Planet',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30),
             ),
-            actions: [IconButton(onPressed: () {}, icon: Icon(Icons.menu))],
+            actions: [
+              IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.menu,
+                    color: Colors.white,
+                  ))
+            ],
+          ),
+          body: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "SPACE DETAILS",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Center(child: Image.asset('assets/earth.png')),
+                  const Text(
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit.Sed do eiusmod tempor incididuntut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquipex ea commodo consequat.",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Center(
+                      child: Container(
+                        height: 50,
+                        width: 100,
+                        decoration: BoxDecoration(
+                            color: Colors.lightGreen,
+                            borderRadius: BorderRadius.circular(5)),
+                        child: Center(
+                          child: Text(
+                            "details",
+                            style: TextStyle(color: Colors.white, fontSize: 25),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  //second screen
+                  Center(
+                      child: Image.asset(
+                    'assets/pluto.png',
+                  )),
+                  Text(
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit.Sed do eiusmod tempor incididuntut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquipex ea commodo consequat.",
+                    style: TextStyle(color: Colors.white),
+                  ),
+
+                  SizedBox(
+                    height: 20,
+                  ),
+
+                  Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(50.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Container(
+                            height: 50,
+                            width: 50,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(50),
+                                color: Colors.lightGreen),
+                          ),
+                          Container(
+                            height: 50,
+                            width: 50,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(50),
+                                color: Colors.redAccent),
+                          ),
+                          Container(
+                            height: 50,
+                            width: 50,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(50),
+                                color: Colors.lightBlue),
+                          ),
+                          Container(
+                            height: 50,
+                            width: 50,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(50),
+                                color: Colors.brown),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+
+                  Image.asset(
+                    'assets/mars.png',
+                  ),
+
+                  Text(
+                      "Lorem ipsum dolor sit amet, consectetur adipiscing elit.Sed do eiusmod tempor incididuntut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquipex ea commodo consequat.",
+                      style: TextStyle(color: Colors.white)),
+
+                  SizedBox(
+                    height: 20,
+                  ),
+
+                  GestureDetector(
+                    onTap: () {},
+                    child: Center(
+                      child: Container(
+                        height: 50,
+                        width: 100,
+                        decoration: BoxDecoration(
+                            color: Colors.lightGreen,
+                            borderRadius: BorderRadius.circular(5)),
+                        child: Center(
+                          child: Text(
+                            "details",
+                            style: TextStyle(color: Colors.white, fontSize: 25),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
         ));
   }
